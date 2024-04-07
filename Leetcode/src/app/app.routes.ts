2094ee3ddc85dce4a808d6ff057cea0cd29f242c
AcciGen <denrov13@gmail.com>
2024-04-07 09:17:02 +0500

    fully done

--- Leetcode/src/app/app.routes.ts
+++ Leetcode/src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: 'home', title: 'Leetcode', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', title: 'Leetcode', component: HomeComponent },
     { path: 'profile', title: 'Profile', component: ProfileComponent },
     { path: '**', title: '404 Not Found', component: NotFoundComponent }
 ];
--- Leetcode/src/app/components/profile/profile.component.ts
+++ Leetcode/src/app/components/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
