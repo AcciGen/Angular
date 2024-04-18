4606c21257121b127dbfeb502e4c08d632b202a4
AcciGen <denrov13@gmail.com>
2024-04-18 21:15:37 +0500

    forgot password page added

--- first-project/src/app/app.routes.ts
+++ first-project/src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { FeaturesComponent } from './components/features/features.component';
 import { MarketplaceComponent } from './components/marketplace/marketplace.component';
 import { CompanyComponent } from './components/company/company.component';
 import { SignUpComponent } from './components/login/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/login/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', title: 'Home', component: HomeComponent },
     { path: 'login', title: 'Login', component: LoginComponent },
     { path: 'signup', title: 'Sign Up', component: SignUpComponent },
+    { path: 'forgotpassword', title: 'Forgot Password', component: ForgotPasswordComponent },
     { path: 'products', title: 'Products', component: ProductsComponent },
     { path: 'features', title: 'Features', component: FeaturesComponent },
     { path: 'marketplace', title: 'Marketplace', component: MarketplaceComponent },
